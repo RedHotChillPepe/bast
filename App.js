4993dbda872cb7b0118ae718a4ae62fa973592e6
Alina <changatasan@gmail.com>
2024-10-01 18:19:40 +0400

    added navigation

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainPage from './pages/MainPage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>INTEDED BLANK PAGE!11!!</Text>
-      <StatusBar style="auto" />
-    </View>
+const Stack = createNativeStackNavigator();
+
+// Объявление доступных страниц
+const AppStack = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Main'>
+        <Stack.Screen name='Main' component={MainPage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Корневой (Root) компонент
+export default function App() {
+  return (
+    <AppStack/>
+  );
+}
+
