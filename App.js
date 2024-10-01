421e67c826fe01385d1b3013ddc218f98b450b23
Alina <changatasan@gmail.com>
2024-10-01 19:05:00 +0400

    added banner mockup added searchbar mockup

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainPage from './pages/MainPage';
 
--- pages/MainPage.js
+++ pages/MainPage.js
@@ -1,21 +1,61 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, TextInput } from 'react-native'
 import React from 'react'
 
 const MainPage = () => {
   return (
     <View style={styles.container}>
-      <Text>MainPage</Text>
+      <View style={styles.content}>
+        <ScrollView style={styles.scrollView}>
+            <View>
+                <View style={styles.banner}>
+                    <Text style={styles.bannerText}>
+                        "Что-то Типа Баннера" - Илья Hotchillipepe
+                    </Text>
+                </View>
+                <View style={styles.searchBar}>
+                    <TextInput placeholderTextColor={"#bfbfbf"}  placeholder='Поиск' style={styles.searchTextInput}/>
+                </View>
+            </View>
+        </ScrollView>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        alignItems:'center',
+        backgroundColor:"#FFF"
     },
+    content: {
+        width:'90%',
+        marginTop:20
+    },
+    scrollView:{
+        width:"100%",
+        height:'100%'
+    },
+    banner:{
+        width:"100%",
+        height:90,
+        backgroundColor:'#0077FF',
+        borderRadius:4
+    },
+    bannerText:{
+        color:"#FFF",
+        marginLeft:8,
+        marginTop:65
+    },
+    searchBar:{
+        marginTop:15
+    },
+    searchTextInput:{
+        backgroundColor:"#F5F5F5",
+        borderRadius:4,
+        height:50,
+        paddingLeft:8
+    }
   });
 
 export default MainPage
