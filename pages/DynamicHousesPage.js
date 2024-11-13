093e1a4caccb26afd323d6a89080dc5d1cd2908f
BahamaMama977 <65508008+BahamaMama977@users.noreply.github.com>
2024-11-13 21:03:41 +0400

    create HouseCard element and import it in MainPage page, DynamicHousePage (replace houses and newHouses blocks)

--- pages/DynamicHousesPage.js
+++ pages/DynamicHousesPage.js
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native'
 import { useApi } from '../context/ApiContext.js'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Image } from 'react-native'
+import HouseCard from '../components/HouseCard.js'
 
 
 const { width } = Dimensions.get('window');
@@ -32,60 +33,25 @@ const DynamicHousesPage = ({route}) => {
         
     
   return (
-    <SafeAreaView style={styles.container}>
-        
+    <View style={styles.container}>
+        <Text>Фильтры</Text>
         <View style={styles.content}>
             <View style={styles.housesView}>
 
-            
-                    {
-                        Object.keys(houses).length != 0 && houses != undefined ?
-                        <FlatList
-                        data={houses}
-                        showsVerticalScrollIndicator={false}
-                        renderItem={({item}, index) => 
-                        <Pressable onPress={() => navigation.navigate("House", {houseId:item.id})}>   
-                            <View style={styles.houseItem}>
-                                <View style={styles.houseImageView}>
-                                    <Image style={styles.houseImage} width={100} height={100} source={{uri:item.photos[0]}}/>
-                                </View>
-                                <View>
-                                    <View style={{flexDirection:'row', alignItems:'flex-end'}}>
-                                        <Text style={styles.houseItemText}>
-                                            {item.price != null && item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽
-                                        </Text>
-                                        <Text style={{fontSize: 12, marginLeft: 8, fontWeight:'200'}}>
-                                            {Math.floor(item.price / item.house_area)}₽/м2
-                                        </Text>
-                                    </View>
-                                    <View style={{flexDirection:"row", marginLeft: 8, marginTop: 2}}>
-                                        <Text style={{fontSize: 14, fontWeight:'600'}}>
-                                            {item.bedrooms}-комн.
-                                        </Text>
-                                        <Text style={{fontSize: 14, fontWeight:'600'}}>
-                                            {item.house_area} м²
-                                        </Text>
-                                        <Text style={{fontSize: 14, fontWeight:'600'}}>
-                                            {item.num_floors} этаж
-                                        </Text>
-                                    </View>
-                                    <Text style={{ fontSize: 12,marginLeft: 8, fontWeight:'200', marginTop: 2, marginBottom: 12}}>
-                                        {item.city}, {item.full_address}
-                                    </Text>
-                                </View>
-                                
-                            </View>
-                        </Pressable> 
-                        }/>
-                        :
-                        <ActivityIndicator
-                        size={"large"}
-                        color={"#32322C"}/>
-                    } 
-            </View> 
+            {houses.length ? (
+            <HouseCard data={houses} 
+                       navigation={navigation} 
+                       itemWidth={Dimensions.get('window').width -32} 
+                       horizontalScroll={false} />
+        ) : (
+          <ActivityIndicator size="large" color="#32322C" />
+        )}
+
+
+            </View>  
         </View>
       
-    </SafeAreaView>
+    </View>
   )
 }
 
@@ -95,6 +61,7 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:'#F5F5F5',
+        alignItems:'flex-start'
     },
     content: {
         
@@ -103,7 +70,7 @@ const styles = StyleSheet.create({
     housesView:{
         width:width,
         
-        flexDirection:'column',
+
         alignItems:'center'
         
     },
