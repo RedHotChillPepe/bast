66b5c45154ebebc5b7e415c1a00ce50cfcb0298a
Alina <changatasan@gmail.com>
2024-12-03 17:44:03 +0400

    fix bad merge favourites page

--- pages/FavouritesPage.js
+++ pages/FavouritesPage.js
@@ -1,9 +1,10 @@
 import { ActivityIndicator, Dimensions, FlatList, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, {useEffect, useState} from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { useIsFocused, useNavigation } from '@react-navigation/native'
 import { useApi } from '../context/ApiContext.js'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import HouseCard from '../components/HouseCard.js'
+import * as SecureStore from 'expo-secure-store';
 
 
 
@@ -12,27 +13,37 @@ const { width } = Dimensions.get('window');
 
 const FavouritesPage = ({route}) => {
 
-    const navigation = useNavigation()
-    const [houses, setHouses] = useState([])
-    const { getAllPosts } = useApi()
+  const navigation = useNavigation()
+  const isFocused = useIsFocused()
 
+  const [houses, setHouses]= useState([])
+  const [isFavs, setIsFavs]= useState(true)
 
-    useEffect(() => {
-      // Проверяем, что массив `houses` пуст, чтобы загрузить данные только один раз
-      if (Object.keys(houses).length === 0) {
-        const loadFromAPI = async () => {
-          try {
-            const response = await getAllPosts();
-            // Убедимся, что `response` является массивом
-            setHouses(Array.isArray(response) ? response : []);
-          } catch (error) {
-            console.error("Error fetching posts:", error);
-            setHouses([]); // Устанавливаем пустой массив при ошибке
-          }
-        };
-        loadFromAPI();
+  const {getPost} = useApi()
+
+  useEffect(() => {
+    const getFavHouses = async () => {
+      const favs = JSON.parse(await SecureStore.getItemAsync("favs"))
+      console.log(favs);
+        
+      if (favs == null) {
+        setIsFavs(false)
+        
+      } else if (Object.keys(favs).length != 0) {
+        const tempHouses = []
+
+        for (let index = 0; index < favs.length; index++) {
+          const result = await getPost(favs[index])
+          const resultJson = JSON.parse(await result.text())
+          tempHouses.push(resultJson.rows[0])          
+        }
+        setHouses(tempHouses)
       }
-    }, [houses, getAllPosts]); // Зависимость от `houses` и `getAllPosts`
+    }
+
+    getFavHouses()
+
+    }, [isFocused]);
         
     
   return (
@@ -40,14 +51,20 @@ const FavouritesPage = ({route}) => {
       <Text style={{fontSize: 24, fontWeight: '600',marginTop: 16, marginBottom: 32, textAlign:'left',alignSelf:'flex-start', marginLeft:32}}>Избранное</Text>
       <View style={styles.content}>
         <View style={styles.housesView}>
-            {Object.keys(houses).length != 0 && houses != undefined ? (
-            <HouseCard data={houses} 
-                       navigation={navigation} 
-                       itemWidth={Dimensions.get('window').width -32} 
-                       horizontalScroll={false} />
-        ) : (
-          <ActivityIndicator size="large" color="#32322C" />
-        )}
+        {Object.keys(houses).length != 0 && houses != undefined 
+          ? 
+          <HouseCard data={houses} 
+            navigation={navigation} 
+            itemWidth={Dimensions.get('window').width -32} 
+            horizontalScroll={false} 
+          />
+          :
+            isFavs 
+            ? 
+            <ActivityIndicator size="large" color="#32322C" /> 
+            :
+            <Text>У вас нет избранных!</Text>
+          }
          </View>  
       </View>      
     </SafeAreaView>
