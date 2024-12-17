e6e67f431cb37f2a967bed8b7a4ed9c03aa02e85
BahamaMama977 <65508008+BahamaMama977@users.noreply.github.com>
2024-12-17 11:18:25 +0400

    add DynamicStoriesPage

--- pages/DynamicStoriesPage.js
+++ pages/DynamicStoriesPage.js
@@ -4,38 +4,65 @@ import { View, Text, Dimensions, SafeAreaView, Pressable } from "react-native";
 const { width, height } = Dimensions.get('window');
 
 const DynamicStoriesPage = () => {
-
+ const margin = 16
 
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{ flex: 1, width: width- 2*margin, marginLeft: margin}}>
 
           {/* ЛОГО */}
-          <Text style={{fontSize: 40, fontWeight:'bold'}}>
+          <Text style={{fontSize: 28, fontWeight:'bold'}}>
             БАСТ
           </Text>
 
-
+         <View style={{marginTop: 56}}>
           {/* Заголовок */}
-          <Text style={{fontSize: 32, fontWeight:'bold'}}>
-            Заголовок
-          </Text>
+            <Text style={{fontSize: 40, fontWeight:'bold', marginBottom: 16}}>
+              Заголовок
+            </Text>
           
-          <Text>
-          Текст новости
-          </Text>
+            <Text style={{fontSize: 20}}>
+              Текст новости Текст новости Текст новости Текст новости Текст новости Текст новости Текст новости
+            </Text>
+          </View>
+         <View>
 
-          <Text>
+         <View style={{marginTop: 60}}>
+            <Text style={{fontSize: 24, fontWeight:'bold'}}>
+              Пункт 1
+            </Text>
+
+            <Text style={{fontSize: 16, marginTop: 12}}>
+              Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1
+            </Text>
+          </View>
+
+          <View>
+          <Text style={{fontSize: 24, fontWeight:'bold', marginTop: 32}}>
             Пункт 1
           </Text>
 
-          <Text>
+          <Text style={{fontSize: 16, marginTop: 12}}>
             Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1
           </Text>
+          </View>
 
-          <Pressable>
-            <Text>Кнопка</Text>
+          <View>
+          <Text style={{fontSize: 24, fontWeight:'bold', marginTop: 32}}>
+            Пункт 1
+          </Text>
+
+          <Text style={{fontSize: 16, marginTop: 12}}>
+            Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1 Текст пункта 1
+          </Text>
+          </View>
+          </View>
+
+        <Pressable style={{backgroundColor:'grey', paddingHorizontal: 12, width: width*0.3, alignItems:'center', paddingVertical: 12, borderRadius: 12, marginBottom: 40, alignSelf: 'flex-end', marginTop: 56}}>
+            <Text style={{fontSize: 18}}>Кнопка</Text>
         </Pressable>
+
+
         </SafeAreaView>
 
 
