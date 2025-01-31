418508d9a9db8fadae75a2de531e17f98783cc26
Alina <changatasan@gmail.com>
2025-01-31 15:30:40 +0400

    added map and #fixme all posts on it

--- pages/SearchMap.js
+++ pages/SearchMap.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import  { YaMap, Marker } from 'react-native-yamap';
+import { Geocoder } from 'react-native-yamap';
+import { useApi } from '../context/ApiContext';
 
-export default function DynamicHousePostPage ({ navigation, route }) {
+export default function SearchMap ({ navigation, route }) {
+  const { getAllPosts} = useApi();
+
+  const [posts, setPosts] = useState([])
+  
+  useEffect(() => {
+    // FIXME Крайне неоптимальный код, срочно поменять
+    const loadMarkers = async () => {
+      const tempHouses = await getAllPosts() // FIXME
+      if (await tempHouses[0].id != undefined) {
+        tempHouses.forEach(house => {
+          if (house.longitude != null || house.latitude != null) {
+            setPosts((prevData) => [...prevData, {point:{lon:parseFloat(house.longitude), lat:parseFloat(house.latitude)}, id:house.id}])
+          } else {
+            const addressString = house.city + " " + house.fulladdress
+            Geocoder.addressToGeo(addressString)
+            .then(({lat, lon}) => {
+              setPosts((prevData) => [...prevData, {point:{lon:lon, lat:lat}, id:house.id}])
+            })
+          }
+          
+        });
+      }
+    }
+    loadMarkers()
+  return () => {}}, [])
+  
+
     return (
-        <SafeAreaView>
-               <YaMap  style={{ flex: 1}}
-                    initialRegion={{
-                        lat: 50,
-                        lon: 50,
-                        zoom: 10,
-                        azimuth: 80,
-                        tilt: 100
-                      }}
-               >
-                 {/* Добавление круга на карту */}
-                 <Marker point={{ lat: 50, lon: 50 }} source={require('../assets/marker.png')} />
-               </YaMap> 
+        <SafeAreaView style={{flex:1}}>
+          <YaMap  style={{width:'100%', height:'100%', alignSelf:'center'}}
+            initialRegion={{lat: 56.84976,lon: 53.20448,zoom: 10}}>
+
+              {
+                Object.keys(posts).length != 0 
+                &&
+                posts.map((post, index) => 
+                  <Marker key={index} onPress={() => navigation.navigate('House', { houseId: post.id })} point={post.point} source={require('../assets/marker.png')} scale={0.4}/>
+                )
+              }
+
+            {/* <Marker onPress={() => navigation.navigate('Error404')} point={{ lat: 56.84976,lon: 53.20448 }} source={require('../assets/marker.png')} scale={0.4}/> */}
+
+          </YaMap> 
         </SafeAreaView>
         )
 }
