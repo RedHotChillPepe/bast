42257b7206168d5be43993647fa7296e669ba99c
BahamaMama977 <65508008+BahamaMama977@users.noreply.github.com>
2025-03-05 16:16:11 +0400

    страница изменения личных данных

--- pages/ChangeAvatarPage.js
+++ pages/ChangeAvatarPage.js
@@ -34,6 +34,8 @@ const ChangeAvatarPage = () => {
   return (
       <ScrollView
       contentContainerStyle={styles.container}>
+      <ScrollView
+      contentContainerStyle={styles.container}>
       <View style={styles.avatarContainer}>
         {avatar ? (
           <Image source={{ uri: avatar }} style={styles.avatarImage} />
@@ -43,13 +45,91 @@ const ChangeAvatarPage = () => {
       </View>
       <Pressable style={styles.button} onPress={() => setModalVisible(true)}>
         <Text style={styles.buttonText}>Сменить аватар</Text>
+      <Pressable style={styles.button} onPress={() => setModalVisible(true)}>
+        <Text style={styles.buttonText}>Сменить аватар</Text>
       </Pressable>
       <AvatarModal
         visible={modalVisible}
         onClose={() => setModalVisible(false)}
         onSelectAvatar={handleAvatarSelect}
       />
-    </View>
+
+      <View style={{height: 32}} />
+
+
+      <View style={styles.block}>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Имя:</Text>
+        </View>
+      
+        <TextInput
+          style={styles.input}
+          placeholder="Имя"
+       // secureTextEntry={true}
+       // value={password}
+       // onChangeText={(text) => setPassword(text)}
+          maxLength={20}
+          placeholderTextColor='rgba(60,60,67, 0.6'
+          fontSize={17}
+          />
+      </View>
+    
+      <View style={styles.block}>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Фамилия:</Text>
+        </View>
+      
+        <TextInput
+          style={styles.input}
+          placeholder="Фамилия"
+       // secureTextEntry={true}
+       // value={password}
+       // onChangeText={(text) => setPassword(text)}
+          maxLength={20}
+          placeholderTextColor='rgba(60,60,67, 0.6'
+          fontSize={17}
+          />
+      </View>
+
+      <View style={styles.block}>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Почта:</Text>
+        </View>
+      
+        <TextInput
+          style={styles.input}
+          placeholder="Почта"
+       // secureTextEntry={true}
+       // value={password}
+       // onChangeText={(text) => setPassword(text)}
+          maxLength={20}
+          placeholderTextColor='rgba(60,60,67, 0.6'
+          fontSize={17}
+          />
+      </View>
+
+      <View style={styles.block}>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Телефон:</Text>
+        </View>
+      
+        <TextInput
+          style={styles.input}
+          placeholder="Телефон"
+       // secureTextEntry={true}
+       // value={password}
+       // onChangeText={(text) => setPassword(text)}
+          maxLength={20}
+          placeholderTextColor='rgba(60,60,67, 0.6'
+          fontSize={17}
+          />
+      </View>
+    
+    <Pressable style={styles.button}>
+      <Text style={styles.buttonText}>Сохранить изменения</Text>
+    </Pressable>
+    <View style={{height: 128}} />
+    </ScrollView>
   );
 };
 
@@ -60,6 +140,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    paddingTop: 16,
     paddingTop: 16,
     backgroundColor: '#f7f7f7',
   },
@@ -78,14 +159,41 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   button: {
+  button: {
     backgroundColor: '#9DC0F6',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 10,
   },
   buttonText: {
+  buttonText: {
     color: '#fff',
     fontSize: 16,
+  },
+
+  block: {
+    width: width - 72, 
+    marginBottom: 12
+  },
+
+  title: {
+    marginBottom:8
+  },
+
+  titleText: {
+    fontSize:20,
+    lineHeight: 25,
+    letterSpacing: -0.45,
+    fontWeight: '500',
+  },
+
+  input: {
+    backgroundColor: 'rgba(120,120,128, 0.12)',
+    height: 40,
+    marginBottom: 12,
+    borderRadius: 12,
+    paddingVertical: 7,
+    paddingHorizontal: 8,
   },
 
   block: {
